fcdc8bf483bfc3567728f586df4a614c7edfc58b
John Freidrich Cinco <jcinco>
2020-07-07 13:40:58 +0800

    Initial placeholders for OAuth handling

--- app/src/main/java/com/jcinco/j5anqlaveassignment/data/providers/auth/OAuthProvider.kt
+++ app/src/main/java/com/jcinco/j5anqlaveassignment/data/providers/auth/OAuthProvider.kt
@@ -1,6 +1,9 @@
 package com.jcinco.j5anqlaveassignment.data.providers.auth
 
+import android.accounts.AccountManager
 import android.content.Context
+import android.os.Bundle
+import android.os.Handler
 import com.jcinco.j5anqlaveassignment.utils.SharedPrefUtil
 
 class OAuthProvider(val context: Context): IAuthProvider {
@@ -17,6 +20,7 @@ class OAuthProvider(val context: Context): IAuthProvider {
         }
     }
 
+
     override fun invalidate(username: String, callback: (success: Boolean) -> Unit?) {
         TODO("Not yet implemented")
     }
@@ -26,6 +30,23 @@ class OAuthProvider(val context: Context): IAuthProvider {
     }
 
 
+    private fun request() {
+        val am = (context.getSystemService(Context.ACCOUNT_SERVICE) as AccountManager)
+        val options = Bundle()
+        val accountType = am.getAccounts().filter { it.type.equals("com.google") }
+        val handler = Handler(context.mainLooper, Handler.Callback {
+            false
+        })
+        am.getAuthToken(
+            accountType.first(),
+            "Access files",
+            true,
+            {},
+            handler
+        )
+
+    }
+    
 
     private fun hasAccessToken():Boolean {
         val sharedPref = SharedPrefUtil.getInstance()
